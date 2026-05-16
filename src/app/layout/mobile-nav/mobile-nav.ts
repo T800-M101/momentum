0c903ecb2ef83a6da56ba629c7b6b6a3e1eca448
Guillermo Morán <tuahil@gmail.com>
2026-05-16 07:56:15 -0600

    add lucien icons

--- src/app/layout/mobile-nav/mobile-nav.ts
+++ src/app/layout/mobile-nav/mobile-nav.ts
@@ -1,12 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ThemeService } from '../../core/services/theme-service';
+import { LucideAngularModule } from 'lucide-angular';
+import {
+  NotebookPen,
+  NotebookTabs,
+  CalendarDays,
+  Settings,
+  SquarePen,
+  Moon,
+  Sun,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-mobile-nav',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './mobile-nav.html',
   styleUrl: './mobile-nav.css',
 })
 export class MobileNav {
   themeService = inject(ThemeService);
+
+  showSettingsMenu = signal(false);
+
+  icons = {
+    NotebookPen,
+    NotebookTabs,
+    CalendarDays,
+    Settings,
+    SquarePen,
+    Moon,
+    Sun,
+  };
+
+  toggleSettingsMenu() {
+    this.showSettingsMenu.update((v) => !v);
+  }
 }
--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,12 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../core/services/theme-service';
+import { LucideAngularModule } from 'lucide-angular';
+import {
+  NotebookPen,
+  NotebookTabs,
+  CalendarDays,
+  Settings,
+  SquarePen,
+  Moon,
+  Sun,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   themeService = inject(ThemeService);
+  icons = {
+    NotebookPen,
+    NotebookTabs,
+    CalendarDays,
+    Settings,
+    SquarePen,
+    Moon,
+    Sun,
+  };
 }
