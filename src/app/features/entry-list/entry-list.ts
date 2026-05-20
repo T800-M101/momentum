2804290baaff2085b913aaf24896317128b0092c
Guillermo Morán <tuahil@gmail.com>
2026-05-19 19:16:54 -0600

    group entries by month

--- src/app/features/entry-list/entry-list.ts
+++ src/app/features/entry-list/entry-list.ts
@@ -1,16 +1,42 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { JournalService } from '../../core/services/journal/journal-service';
 import { JournalCard } from '../../shared/journal/journal-card/journal-card';
+import { IconsService } from '../../core/services/icons/icons-service';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-entry-list',
-  imports: [JournalCard],
+  imports: [JournalCard, LucideAngularModule],
   templateUrl: './entry-list.html',
   styleUrl: './entry-list.css',
 })
 export class EntryList implements OnInit {
   journalService = inject(JournalService);
   entries = this.journalService.entries;
+
+  private iconsService = inject(IconsService);
+  icons = this.iconsService.icons;
+
+  groupedEntries = computed(() => {
+    const entries = this.journalService.entries();
+    const groups: { [key: string]: any[] } = {};
+
+    entries.forEach(entry => {
+      const entryDate = new Date(entry.date);
+
+      const monthYear = entryDate.toLocaleString('en-US', { month: 'short', year: 'numeric' });
+
+      if (!groups[monthYear]) {
+        groups[monthYear] = [];
+      }
+      groups[monthYear].push(entry);
+    });
+
+    return Object.keys(groups).map(key => ({
+      monthYear: key,
+      entries: groups[key]
+    }));
+  });
 
   ngOnInit() {
     this.journalService.loadEntries();
